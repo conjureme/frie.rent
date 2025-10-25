828ef75b55b9df7b60b052df062fc310362176a7
tyler <209597026+conjureme@users.noreply.github.com>
2025-10-25 18:43:39 -0500

    fix: z-index stacking, links should work now

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,21 @@ function App() {
         color='#4e5a63'
         refresh
       />
-      <div className='fixed inset-0 w-full h-full z-50'>
+      <div className='fixed inset-0 w-full h-full z-20'>
         <Plushie gravity={[0, -40, 0]} position={[0, 0, 20]} />
       </div>
-      <div className='fixed bottom-4 left-4 z-10 flex flex-col gap-3'>
+      <div className='fixed bottom-16 left-4 z-10'>
         <ActivityCard />
+      </div>
+      <div className='fixed bottom-4 left-4 z-30'>
         <SocialLinks />
       </div>
-      <div className='fixed bottom-4 right-4 z-10'>
+      <div className='fixed bottom-4 right-4 z-30'>
         <a
           href='https://sketchfab.com/3d-models/frieren-plushie-209c79c641164b38a81e145b6af3f890'
           target='_blank'
           rel='noopener noreferrer'
-          className='text-base-content/60 hover:text-base-content transition-colors text-sm'
+          className='text-base-content/60 hover:text-base-content transition-colors text-sm underline'
         >
           frieren-plushie.obj
         </a>
